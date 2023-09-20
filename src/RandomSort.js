8b74b7564cdfe872f03628ac8be4d78cc628d2d7
Abbas <a.shahzadeh@gmail.com>
2023-09-20 21:07:39 +1000

    Improved the random sort algorithm

--- src/RandomSort.js
+++ src/RandomSort.js
@@ -1,10 +1,18 @@
 export default function RandomSort(words, seed) {
+    // make a copy so that we do not modify the input array
+    let remainingWords = [...words];
+
+    let orderedWords = [];
     while (orderedWords.length < words.length) {
-        let index = Math.floor(random(seed++) * words.length);
-        let theWord = words[index];
-        if (!orderedWords.includes(theWord)) {
-            orderedWords.push(theWord);
-        }
+        // randomly pick one word from the remaining words
+        let index = Math.floor(random(seed++) * remainingWords.length);
+        let theWord = remainingWords[index];
+
+        // add the picked word to the output list
+        orderedWords.push(theWord);
+
+        // remove it from the remaining words
+        remainingWords.splice(index, 1);
     }
 
     return orderedWords;
